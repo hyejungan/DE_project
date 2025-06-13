17aca7ef1f2daa2c43377e8ae77ed5f65c715676
da Ani <qinama880@naver.com>
2025-06-13 18:44:44 +0900

    data-upload

--- etl/load_data_1.py
+++ etl/load_data_1.py
@@ -7,11 +7,14 @@ DATA_DIR = os.path.join(BASE_DIR, 'data')
 PROCESSED_DIR = os.path.join(BASE_DIR, 'processed')
 
 file_paths = glob.glob(os.path.join(DATA_DIR, '*2020*.xlsx'))
-dfs = [pd.read_excel(file, skiprows=3).iloc[:,:9] for file in file_paths]
-df = pd.concat(dfs, ignore_index=True)
-df.to_csv('final_data_1.csv', index=False, encoding='utf-8-sig')
-df = pd.read_csv('final_data_1.csv')
-print("CSV 파일 저장 완료!")
+# dfs = [pd.read_excel(file, skiprows=3).iloc[:,:9] for file in file_paths]
+# df = pd.concat(dfs, ignore_index=True)
+# df.to_csv('final_data_1.csv', index=False, encoding='utf-8-sig')
+# df = pd.read_csv('final_data_1.csv')
+# print("CSV 파일 저장 완료!")
 
 # import os
 # print(os.getcwd())
+for file in file_paths:
+    dfs = pd.read_excel(file).iloc[:,:9]
+    dfs.to_excel(file,index=False)
